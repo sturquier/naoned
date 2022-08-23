5464eeb776c7a236c16c2727a43a7ce46c95eb60
sturquier <turquiersimon@hotmail.fr>
2022-08-23 17:15:02 +0200

    Install & setup google maps

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Map extends StatelessWidget {
   const Map({Key? key}) : super(key: key);
 
+  static const CameraPosition initialCameraPosition =
+      CameraPosition(target: LatLng(47.2382327, -1.5603345), zoom: 12);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Text('Map', style: Theme.of(context).textTheme.headline1)));
+    return const Scaffold(
+        body: GoogleMap(
+      initialCameraPosition: initialCameraPosition,
+    ));
   }
 }
